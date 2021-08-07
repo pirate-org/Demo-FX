018ab67c731bf28bf7ee3fd0e98dc1efd19ae148
Udara Deshan <udaradeshan.ud@gmail.com>
2021-08-07 11:57:39 +0530

    create dashboard

---
+++ src/lk/pirate/demoFx/Main.java
@@ -0,0 +1,29 @@
+package lk.pirate.demoFx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().
+                    getResource("/lk/pirate/demoFX/view/dashbord.fxml")));
+
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/lk/pirate/demoFx/controller/DashbordController.java
@@ -0,0 +1,22 @@
+/*
+ * *
+ *  * @author : Udara Deshan <udaradeshan.ud@gmail.com>
+ *  * @since : 08/07/2021
+ *  *
+ */
+
+package lk.pirate.demoFx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DashbordController {
+    @FXML
+    private Button btnCustomer;
+
+    @FXML
+    void navigateToCustomer(ActionEvent event) {
+        System.out.println('s');
+    }
+}
